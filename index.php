9247f1401cbc4ae22dacbd88040792c8d49cb8c1
Sergio Salomao <salomao.leite@hotmail.com>
2019-05-08 22:48:20 -0300

    ajuste

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <?php
-
 #Script para leitura de arquivo CSV transformar em json
-$handle = fopen("arquivo.csv", "r");
+$handle = fopen("apreensoes_para_regularizar.csv", "r");
 $row = 0;
-while ($line = fgetcsv($handle, 1000, ";")) { //dependendo do arquivo usar "," no delimiter
-    if ($row++ == 0) {
-            continue;
-        }
 
-    $dados[] =
-        [
-            'id' => $line[0],
-            'nome' => $line[1],
-            'telefone' => $line[2],
-            'endereco' => $line[3],
-            //'json' => $line[4]
-         ];
+#Definir inicio e fim da leitura
+$inicio = 1300;
+$fim =1335;
+
+while ($line = fgetcsv($handle, 1000, ",")) {
+    if ($row++ == 0) {
+        continue;
     }
+
+    if (($row >= $inicio) && ($row <= $fim)) {
+        $dados[] =
+            [
+                'numero_apreensao' => $line[0],
+                'numero_leilao' => $line[5],
+                'data_leilao' => $line[6],
+                'numero_lote' => $line[7],
+                'classificacao' => $line[8]
+            ];
+    }
+}
+
 echo $json = json_encode($dados);
 fclose($handle);
-
-
-
-
